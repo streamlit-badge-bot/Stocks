dc8273da48ee4fb0512438afeecc93ad605c80c7
Monica Bustamante <lilianalambda@gmail.com>
2020-11-20 10:16:02 -0800

    'update'

--- home.py
+++ home.py
@@ -256,7 +256,7 @@ def Portfolio():
     #     """)
     
     stockStarData = st.sidebar.date_input("Select Date when you started to investing:")
-    company = tickerSymbol1 = st.sidebar.multiselect("Select only 5 Companies to create the Portfolio",  (df['Symbol']), ' ,'.join({'Symbol':['AAPL', 'AMZN', 'AA', 'A', 'AC']}))
+    company = tickerSymbol1 = st.sidebar.multiselect("Select only 5 Companies to create the Portfolio",  (df['Symbol']))
     button_clicked = st.sidebar.button("GO")
     if company:
         def getmyportfolio(stock=tickerSymbol1, start=stockStarData, end=None):

6079974c5f81a9bb37f7941258c29da0e24a3826
Monica Bustamante <lilianalambda@gmail.com>
2020-11-21 22:03:17 -0800

    'update'

--- home.py
+++ home.py
@@ -97,6 +97,7 @@ def Home():
         - Financial Statement and Forecast.
 
         - Profiling each company.
+        
 
 
         ---
@@ -117,7 +118,7 @@ def Home():
             <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
             <link href="static/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-            <footer class="background-color: red">
+            <footer class="background-color: grey">
                 <div class="container" id="About App">
                 <div class="row">
                     <div class="col l6 s12">
